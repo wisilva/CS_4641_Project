6316867aee4963f3317d4966f9f523d56f42f9fe
PieBob851 <carter.stephan1950@gmail.com>
2023-07-17 06:40:59 -0400

    adjusted normalization and cutoff
    Normalization done via a sigmoid function.
    Cutoff loops through 0.01 to 1.0 to find an optimal accuracy

--- regression.py
+++ regression.py
@@ -24,31 +24,39 @@ model = Ridge()
 model.fit(X_train, y_train)
 
 train_predict = model.predict(X_train)
-y_train_hat = np.where((train_predict/max(train_predict)) > 0.5, 1, 0)
-
-train_accuracy = accuracy_score(y_train, y_train_hat)
-
 test_predict = model.predict(X_test)
-y_test_hat = np.where((test_predict/max(test_predict)) > 0.5, 1, 0)
 
-test_accuracy = accuracy_score(y_test, y_test_hat)
+train_predict_sigmoid = 1 / (1 + np.exp(-train_predict))
+test_predict_sigmoid = 1 / (1 + np.exp(-test_predict))
+for i in range(100):
+    cutoff = 0.01 + i * 0.01
 
-print(train_accuracy)
-print(test_accuracy)
+    print("cutoff: ", cutoff)
+    
+    y_train_hat = np.where(train_predict_sigmoid > cutoff, 1, 0)
 
-N = y_test.shape[0]
-y_n = y_test==0
-y_nh = y_test_hat==0
-y_p = y_test==1
-y_ph = y_test_hat==1
+    train_accuracy = accuracy_score(y_train, y_train_hat)
+    print("Train accuracy:", train_accuracy)
 
-true_negative = np.sum(y_n & y_nh)
-true_positive = np.sum(y_p & y_ph)
-false_negative = np.sum(y_p & y_nh)
-false_positive = np.sum(y_n & y_ph)
+    y_test_hat = np.where(test_predict_sigmoid > cutoff, 1, 0)
 
-print('True negative:', true_negative)
-print('True positive:', true_positive)
-print('False negative:', false_negative)
-print('False positive:', false_positive)
-print(N)
+    test_accuracy = accuracy_score(y_test, y_test_hat)
+
+    print("Test accuracy: ", test_accuracy)
+
+    N = y_test.shape[0]
+    y_n = y_test==0
+    y_nh = y_test_hat==0
+    y_p = y_test==1
+    y_ph = y_test_hat==1
+
+    true_negative = np.sum(y_n & y_nh)
+    true_positive = np.sum(y_p & y_ph)
+    false_negative = np.sum(y_p & y_nh)
+    false_positive = np.sum(y_n & y_ph)
+
+    print('True negative:', true_negative)
+    print('True positive:', true_positive)
+    print('False negative:', false_negative)
+    print('False positive:', false_positive)
+    print(N)
